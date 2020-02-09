d68e0812e37ff60bcbcc3410364a4b7e0072b581
aglegaspi <lex83nyc@gmail.com>
2020-02-09 14:47:29 -0500

    set up programmatic programming

---
+++ iOS_GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_GHFollowers
+//
+//  Created by Alex 6.1 on 2/9/20.
+//  Copyright © 2020 aglegaspi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
