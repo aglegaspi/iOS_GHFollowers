4f555b8ae3cd053009dab6fb87f58afa38143260
aglegaspi <lex83nyc@gmail.com>
2020-02-10 05:51:17 -0500

    refactor network manager

--- iOS_GHFollowers/Screens/FollowerListVC.swift
+++ iOS_GHFollowers/Screens/FollowerListVC.swift
@@ -11,20 +11,21 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!.rawValue, buttonTitle: "Ok")
-                return
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (result) in
+            switch result {
+            case .success(let followers):
+                print("Followers count: \(followers.count)")
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
-            
-            print("Followers count: \(followers.count)")
-            print(followers)
         }
     }
     
@@ -32,5 +33,5 @@ class FollowerListVC: UIViewController {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
-
+    
 }
