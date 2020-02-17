ef44a15e8fad0a336933cba64688df8bf0ccf3d7
aglegaspi <lex83nyc@gmail.com>
2020-02-16 19:34:54 -0500

    add activity indictor and view

--- iOS_GHFollowers/Screens/FollowerListVC.swift
+++ iOS_GHFollowers/Screens/FollowerListVC.swift
@@ -53,9 +53,13 @@ class FollowerListVC: UIViewController {
     }
     
     func getFollowers(username: String, page: Int) {
+        
+        showLoadingView()
+        
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] (result) in
+            //#warning("Call Dismiss")
             guard let self = self else { return } // weak returns a optional so we guard
-            
+            self.dismissLoadingView()
             switch result {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
